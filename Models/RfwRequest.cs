7bc849436effc299a68096187349f73955edc661
kj666 <karthi-kan@hotmail.com>
2019-10-17 18:44:37 -0400

    Create RFWRequest and Workload Model

---
+++ Models/RfwRequest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Workload.Models
+{
+    public class RfwRequest
+    {
+        public string RfwID { get; set; }
+
+        /// <summary>
+        /// DVD or NDBench
+        /// </summary>
+        public string BenchmarkType { get; set; }
+
+        /// <summary>
+        /// Type of workload Metric (CPU or NetworkIn or NetworkOut or Memory)
+        /// </summary>
+        public string WorloadMetric { get; set; }
+
+        public long BatchUnit { get; set; }
+
+        public long BatchId { get; set; }
+
+        public long BatchSize { get; set; 
+
+    }
+}
---
+++ Models/Workload.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Workload.Models
+{
+    public class Workload
+    {
+        public Workload() { }
+
+        public double CPUUtilization_Average { get; set; }
+
+        public double NetworkIn_Average { get; set; }
+
+        public double NetworkOut_Average { get; set; }
+
+        public double MemoryUtilization_Average { get; set; }
+
+    }
+}
