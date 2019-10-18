99d08b952b5b502260fb92e2614a7f0cb53e20b3
kj666 <karthi-kan@hotmail.com>
2019-10-17 23:43:27 -0400

    Added Sample Respnse Controller

---
+++ Models/RfwResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Workload.Models
+{
+    public class RfwResponse
+    {
+        public string RfwID { get; set; }
+        
+        public long LastBatchId { get; set; }
+
+        public List<Workload> Samples { get; set; }
+    }
+}
